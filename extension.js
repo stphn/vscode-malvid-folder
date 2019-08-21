c391eec04f854027791e0c1a858e9b48086c0360
Stephane Goeuriot <sgoeuriot@comwrap.com>
2019-08-21 08:24:10 +0200

    first commit

---
+++ extension.js
@@ -0,0 +1,66 @@
+'use strict'
+const vscode = require('vscode')
+const paramCase = require('change-case').paramCase
+const utils = require('./utils')
+const { logger, generators } = utils
+
+function activate(context) {
+
+	let createComponent = (uri, type) => {
+
+		console.log('Create-malvid-component activated...')
+
+		new Promise(resolve =>
+			vscode.window
+				.showInputBox({
+					prompt: 'Enter component name'
+				})
+				.then(inputValue => resolve(inputValue))
+		)
+		.then(val => {
+			if (val.length === 0) {
+				logger('error', 'Component name can not be empty!')
+				throw new Error('Component name can not be empty!')
+			}
+
+			let componentName = paramCase(val)
+			let componentDir = generators.createComponentDir(uri, componentName)
+
+			return Promise.all([
+				generators.createComponent(componentDir, componentName),
+				generators.createDataJs(componentDir, componentName),
+				type === 'config' ? generators.createConfigJs(componentDir, componentName) : undefined
+			])
+		})
+		.then(
+			() => logger('success', 'Malvid component successfully created!'),
+			err => logger('error', err.message)
+		)
+	}
+
+	const componentsList = [
+		{
+			type: 'config',
+			commandID: 'extension.createMalvidComponentWithConfig'
+		},
+		{
+			type: 'basic',
+			commandID: 'extension.createMalvidComponent'
+		}
+	];
+
+	componentsList.forEach(comp => {
+		let type = comp.type
+		let disposable = vscode.commands.registerCommand(comp.commandID, uri => {
+			createComponent(uri, type)
+		});
+		context.subscriptions.push(disposable)
+	});
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
---
+++ utils.js
@@ -0,0 +1,100 @@
+'use strict'
+const vscode = require('vscode')
+const fse = require('fs-extra')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const snake = require('change-case').snake
+
+function logger(type, msg = '') {
+	switch (type) {
+		case 'success': return vscode.window.setStatusBarMessage(`Success: ${msg}`, 5000)
+		case 'warning': return vscode.window.showWarningMessage(`Warning: ${msg}`)
+		case 'error': return vscode.window.showErrorMessage(`Failed: ${msg}`)
+	}
+}
+
+module.exports = {
+	logger,
+	generators: {
+		templatesDir: path.join(__dirname, '/templates'),
+
+		createFile: (file, data) =>
+		new Promise(resolve => {
+			let output = fse.outputFile(file, data)
+			resolve(output)
+		}),
+
+		resolveWorkspaceRoot: path =>
+		path.replace('${workspaceFolder}', vscode.workspace.rootPath),
+
+		createComponentDir: function(uri, componentName) {
+			let contextMenuSourcePath
+
+			if (uri && fs.lstatSync(uri.fsPath).isDirectory()) {
+				contextMenuSourcePath = uri.fsPath
+			} else if (uri) {
+				contextMenuSourcePath = path.dirname(uri.fsPath)
+			} else {
+				contextMenuSourcePath = vscode.workspace.rootPath
+			}
+
+			const folders = glob.sync('components', {
+				cwd: contextMenuSourcePath
+			})
+
+			let componentDir
+
+			if (folders.length === 0) componentDir = `${ contextMenuSourcePath }/${snake(componentName)}`
+
+			else componentDir = `${ contextMenuSourcePath }/${ folders[0] }/${snake(componentName)}`
+
+			fse.mkdirsSync(componentDir)
+
+			return componentDir
+		},
+
+		createComponent: function(componentDir, componentName) {
+			let templateFileName = this.templatesDir + `/njk.template`
+
+			const compName = snake(componentName)
+
+			let componentContent = fs
+				.readFileSync(templateFileName)
+				.toString()
+				.replace(/{componentName}/g, compName)
+
+			let filename = `${componentDir}/${compName}.njk`
+
+			return this.createFile(filename, componentContent)
+		},
+
+		createDataJs: function(componentDir, componentName) {
+			let templateFileName = this.templatesDir + `/data.template`
+
+			const compName = snake(componentName)
+
+			let componentContent = fs
+				.readFileSync(templateFileName)
+				.toString()
+				.replace(/{componentName}/g, compName)
+
+			let filename = `${componentDir}/${compName}.data.js`
+
+			return this.createFile(filename, componentContent)
+		},
+		createConfigJs: function(componentDir, componentName) {
+			let templateFileName = this.templatesDir + `/config.template`
+			const compName = snake(componentName)
+
+			let componentContent = fs
+				.readFileSync(templateFileName)
+				.toString()
+				.replace(/{componentName}/g, compName)
+
+			let filename = `${componentDir}/${compName}.config.js`
+
+			return this.createFile(filename, componentContent)
+		}
+	}
+};
